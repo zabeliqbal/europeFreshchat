564de8d1af182d961c49702cb61d07f0b51af09e
ebofi <masterstanlystephen@gmail.com>
2020-07-25 12:39:08 +0530

    Update freshchatPlugin.java
    CHanged to IN Server

--- src/android/freshchatPlugin.java
+++ src/android/freshchatPlugin.java
@@ -84,7 +84,7 @@ public class freshchatPlugin extends CordovaPlugin {
                     freshchatConfig = new FreshchatConfig(appId,appKey);
 
                 
-                        freshchatConfig.setDomain("https://msdk.au.freshchat.com");
+                        freshchatConfig.setDomain("https://msdk.in.freshchat.com");
                     
                     if(initArgs.has("cameraCaptureEnabled")) {
                         freshchatConfig.setCameraCaptureEnabled(initArgs.getBoolean("cameraCaptureEnabled"));
